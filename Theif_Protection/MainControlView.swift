9e097e4a026b109f61064d9f1c86bf73e9fb000d
Lighthit <poomipatsubinmit@gmail.com>
2025-05-16 17:13:25 +0700

    V1.0.1 : Make UI and Logic UI

---
+++ Theif_Protection/MainControlView.swift
@@ -0,0 +1,18 @@
+//
+//  MainControlView.swift
+//  Theif_Protection
+//
+//  Created by poomipat Makornwattana on 16/5/2568 BE.
+//
+
+import SwiftUI
+
+struct MainControlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainControlView()
+}
